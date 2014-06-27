74825545265908f9cd8e49ac4319db92f2bded07
Hari KT <kthari85@gmail.com>
2014-06-27 16:26:42 +0530

    Initial v2 bundle

--- config/Common.php
+++ config/Common.php
@@ -8,9 +8,11 @@ class Common extends Config
 {
     public function define(Container $di)
     {
-        $di->params['Aura\Input\Form']['filter'] = $di->lazyNew('FOA\Filter_Input_Bundle\Filter');        
+        $di->params['Aura\Input\Form']['filter'] = $di->lazyNew('FOA\Filter_Input_Bundle\Filter');
+        $di->params['Aura\Input\Fieldset']['builder'] = $di->lazyNew('Aura\Input\Builder');
+        $di->params['Aura\Input\Fieldset']['filter'] = $di->lazyNew('Aura\Input\Filter');
     }
-    
+
     public function modify(Container $di)
     {
     }
